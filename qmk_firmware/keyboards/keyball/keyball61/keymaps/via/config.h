75d18e738dd86fd8978a2240cf70ba048b7f315f
atusy <30277794+atusy@users.noreply.github.com>
2022-06-13 22:20:31 +0900

    define TAPPING_TERM 150

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -18,6 +18,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define TAPPING_TERM 150
+
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
