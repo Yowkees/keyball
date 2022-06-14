3cb790bb6dbd4b39d4e6704e2a2d6f9d68e50bc4
atusy <30277794+atusy@users.noreply.github.com>
2022-06-14 17:24:33 +0900

    set TAPPING_TERM 100

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 100
 
 #pragma once
 
