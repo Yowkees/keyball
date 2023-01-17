15b3af6be5add9c692612aef0ab3ffbfedbf0b84
Akihiro Okuno <choplin.choplin@gmail.com>
2023-01-17 21:26:48 +0900

    Remove deprecated configurations

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -76,5 +76,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
