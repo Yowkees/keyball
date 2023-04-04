7e8533d85e9da70808bde544d5ecde7cf6c1db26
redgosho <redgosho@gmail.com>
2023-04-04 23:30:50 +0900

    RGBLIGHTをONに設定

--- qmk_firmware/keyboards/keyball/keyball61/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/rules.mk
@@ -33,7 +33,7 @@ QUANTUM_LIB_SRC += spi_master.c # Optical sensor use SPI to communicate
 MOUSEKEY_ENABLE = no
 
 # Enabled only one of RGBLIGHT and RGB_MATRIX if necessary.
-RGBLIGHT_ENABLE = no        # Enable RGBLIGHT
+RGBLIGHT_ENABLE = yes        # Enable RGBLIGHT
 RGBLIGHT_DRIVER = WS2812
 RGB_MATRIX_ENABLE = no      # Enable RGB_MATRIX (not work yet)
 RGB_MATRIX_DRIVER = WS2812
