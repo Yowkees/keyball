15a0c1cbe9ad7a7aac90264d15a0de10c5fc72f9
Algus Dark <algus.dark@gmail.com>
2023-06-01 22:15:13 +0900

    Remove USB detect and fix typo in README
    - PRO Micro uses the VBUS for master/slave delegation
    - README had wrong latest tested QMK information

--- qmk_firmware/keyboards/keyball/keyball44/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/config.h
@@ -39,8 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Split parameters
 #define SOFT_SERIAL_PIN         D2
 #define SPLIT_HAND_MATRIX_GRID  F7, D4
-#define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT       500
 
 #define SPLIT_TRANSACTION_IDS_KB KEYBALL_GET_INFO, KEYBALL_GET_MOTION, KEYBALL_SET_CPI
 
