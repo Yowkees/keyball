8f79836b4f4ba9fb66cd5d54d4ffab9699b8b3a2
Daisuke <daisuke.k4@gmail.com>
2023-06-18 09:45:14 +0900

    KEY_OVERRIDE_ENABLEのコードを追加（コメントアウト中）

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/rules.mk
@@ -2,8 +2,12 @@ RGBLIGHT_ENABLE = yes
 
 OLED_ENABLE = yes
 
-# AUTO_SHIFT_ENABLE = yes
-
-# COMBO_ENABLE = yes
-
 VIA_ENABLE = yes
+
+
+### 使っていないもの
+### 以下はメモリ不足のため、VIAをOFFにする必要がある
+# KEY_OVERRIDE_ENABLE = yes
+
+# AUTO_SHIFT_ENABLE = yes
+# COMBO_ENABLE = yes
