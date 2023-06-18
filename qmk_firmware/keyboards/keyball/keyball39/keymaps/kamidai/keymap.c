27d1b1cd31cfb26644a35439cab59c8069b47775
Daisuke <daisuke.k4@gmail.com>
2023-06-19 08:46:03 +0900

    キーマップ変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
@@ -50,13 +50,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_A     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_Z     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINUS ,
     KC_V     , KC_X     , KC_C     ,LT(2,KC_W),LT(3,KC_B),                            KC_N     , KC_M     , KC_COMMA , KC_DOT   , KC_SLASH ,
-    SFT_T(KC_LANG2),ALT_T(KC_F13),LT(1,KC_ESC),CMD_T(KC_SPACE),CTL_T(KC_F14),KC_Q,    KC_BSPC, KC_ENT,_______,_______,_______, LT(1,KC_LANG1)
+    SFT_T(KC_LANG2),ALT_T(KC_F13),LT(1,KC_ESC),CMD_T(KC_SPACE),CTL_T(KC_F14),KC_Q,     KC_BSPC, KC_ENT,_______,_______,_______, LT(1,KC_LANG1)
   ),
 
   [1] = LAYOUT_universal(
-    _______  , G(KC_A)  , G(KC_F)  , _______  , _______  ,                            _______  , _______  , _______  , _______  , _______  ,
-    _______  , __jj_____  , KC_LEFT  , KC_RIGHT , KC_SPACE ,                            G(KC_G)  , KC_DOWN  , KC_UP    , G(KC_V)  , _______  ,
-    _______  , _______  ,C(KC_LEFT), _______ ,C(KC_RIGHT),                            G(KC_Z)  , G(KC_C)  , G(KC_X)  , _______  , G(KC_SLASH),
+    _______  , G(KC_A)  , _______  , _______  , _______  ,                            _______  , _______  , KC_F23   , KC_F24   , _______  ,
+    _______  , _______  , KC_LEFT  , KC_RIGHT , KC_SPACE ,                            _______  , KC_DOWN  , KC_UP    , G(KC_V)  , _______  ,
+    _______  , _______  , _______ ,C(KC_LEFT),C(KC_RIGHT),                            G(KC_Z)  , G(KC_C)  , G(KC_X)  , _______  , G(KC_SLASH),
     _______, _______, _______, _______, _______, _______ ,                            _______  , _______  ,_______,_______,_______, _______
   ),
 
