dfdd1087a850ba8e51fc32ae60d67556846e6755
Daisuke <daisuke.k4@gmail.com>
2023-06-24 08:05:43 +0900

    未使用関数をコメントアウト

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/utils/functions.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/utils/functions.h
@@ -7,9 +7,9 @@ int16_t my_abs(int16_t num) {
 }
 
 // 自前の符号を返す関数。 Function to return the sign.
-int16_t mmouse_move_y_sign(int16_t num) {
-  if (num < 0) {
-    return -1;
-  }
-  return 1;
-}
+// int16_t mmouse_move_y_sign(int16_t num) {
+//   if (num < 0) {
+//     return -1;
+//   }
+//   return 1;
+// }
