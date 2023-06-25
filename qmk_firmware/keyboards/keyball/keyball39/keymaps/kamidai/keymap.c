e50118c9da05b1c522d0fe63dbf2d80604f761a0
Daisuke <daisuke.k4@gmail.com>
2023-06-25 13:12:58 +0900

    キーマップ変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     MT(MOD_LGUI | MOD_LCTL, KC_TAB), KC_A, KC_E, KC_R, KC_T,                          KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_X     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINUS ,
-    KC_C     , KC_V     , KC_W     ,LT(2,KC_Z),LT(3,KC_B),                            KC_N     , KC_M     , KC_COMMA , KC_DOT   , KC_SLASH ,
+    KC_W     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINUS ,
+    KC_X     , KC_V     , KC_C     ,LT(2,KC_Z),LT(3,KC_B),                            KC_N     , KC_M     , KC_COMMA , KC_DOT   , KC_SLASH ,
     SFT_T(KC_LANG2),ALT_T(KC_F13),LT(1,KC_ESC),CMD_T(KC_SPACE),CTL_T(KC_F14),KC_Q,     KC_BSPC, KC_ENT,_______,_______,_______, LT(1,KC_LANG1)
   ),
 
