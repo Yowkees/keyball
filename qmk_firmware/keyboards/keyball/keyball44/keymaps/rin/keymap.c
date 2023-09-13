5aa7c1be779b01ae6c8e6260975fdbadcde771ca
Akira Hayashi <smxshxishxad@me.com>
2023-09-14 07:08:16 +0900

    Set default layer to 0

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
@@ -158,6 +158,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
+void keyboard_post_init_user(void) {
+    default_layer_set(0);
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
