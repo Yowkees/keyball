4ef05a5434fcd22c0930f592e842a84459e196a6
eowiz <eowiz@proton.me>
2023-11-29 00:33:59 +0900

    feat: update keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/someone/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/someone/keymap.c
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
-#include <string.h>
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -27,16 +26,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                   KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_EQUAL , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                   KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                   KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-    KC_ESC   , _______  , KC_LALT  , _______  , KC_BSPC  , KC_LCTL  , KC_LGUI  ,              KC_ENT  , KC_SPC   , _______ , _______   , _______  , _______  , _______
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
+    KC_ESC   , _______  , _______  , KC_LALT  , KC_BSPC  , KC_LCTL  , KC_LGUI  ,              KC_ENT  , KC_SPC   , KC_NO    , KC_NO    , KC_NO    , _______  , MO(1)
   ),
 
+  // mouse layer
   [1] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
+    KC_NO    , KC_NO    , KC_NO         , KC_NO         , KC_NO         , KC_NO    ,                                   KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO         , LAG(KC_F)     , KC_NO         , KC_NO    ,                                   KC_NO    , KC_BTN1  , KC_UP    , KC_BTN2  , KC_NO    , SCRL_MO  ,
+    KC_NO    , KC_NO    , LAG(KC_LEFT)  , LCTL(KC_DOWN) , LAG(KC_RGHT)  , KC_NO    ,                                   KC_NO    , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO    , LCTL(KC_LEFT) , KC_NO         , LCTL(KC_RGHT) , KC_NO    , KC_NO    ,             KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
+    KC_NO    , KC_NO    , KC_NO         , KC_NO         , KC_NO         , KC_NO    , KC_NO    ,             KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , _______
   ),
 };
 // clang-format on
@@ -49,7 +49,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 #ifdef OLED_ENABLE
 
-#    include "lib/oledkit/oledkit.h"
+#include "lib/oledkit/oledkit.h"
 
 void oledkit_render_info_user(void) {
     keyball_oled_render_keyinfo();
