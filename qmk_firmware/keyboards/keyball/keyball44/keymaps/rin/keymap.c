8530fdd719ae351e306f90623807c61d606d48ca
Akira Hayashi <smxshxishxad@me.com>
2024-01-01 06:55:55 +0900

    Map LCTL_NICOLA to left pinky

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/rin/keymap.c
@@ -425,9 +425,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NICOLA] = LAYOUT_universal(
     LGUI_T(KC_TAB),  KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,                                  KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH,
-    LCTL_T(KC_ESC),  KC_A,    KC_O,    KC_E,   KC_U, KC_I,                                  KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,
+    LCTL_NICOLA,     KC_A,    KC_O,    KC_E,   KC_U, KC_I,                                  KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS,
     LSFT_T(KC_ESC),  KC_SCLN, KC_Q,    KC_J,   KC_K, KC_X,                                  KC_B, KC_M, KC_W, KC_V, KC_Z, RSFT_T(KC_ENT),
-    LSFT_T(KC_CAPS), KC_LALT, NICOLA_LSFT, LT(2,KC_SPC), LCTL_NICOLA,   TO_DVORAK, NICOLA_RSFT, _______, _______, KC_BTN1
+    LSFT_T(KC_CAPS), KC_LALT, NICOLA_LSFT, LT(2,KC_SPC), LT(2,KC_ENT),           TO_DVORAK, NICOLA_RSFT, _______, _______, KC_BTN1
   ),
   [DVORAK] = LAYOUT_universal(
     _______, _______, _______, _______, _______, _______,                                   _______, _______, _______, _______, _______, _______,
