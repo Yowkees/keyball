68b1dfbc8d13bf3fcf97da4355d04c833f9a0fbd
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-03-23 16:56:30 +0900

    keball61 uses layer#2 as mouse layer

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
@@ -36,4 +36,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_DEFAULT_LAYER 2
