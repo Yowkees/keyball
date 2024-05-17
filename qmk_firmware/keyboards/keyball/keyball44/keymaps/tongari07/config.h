932b0eb84218a72b7e2a84f32aa31f537eeef351
Naoto Togawa@こまちょ <52874708+tongari07@users.noreply.github.com>
2024-05-17 18:15:54 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tongari07/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tongari07/config.h
@@ -47,7 +47,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TRACKBALL_SCROLL_DIVIDER 30
 
 // Time until layer switch key is enabled
-#define TAPPING_TERM 150
+#define TAPPING_TERM 180
 
 // Add layer
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
