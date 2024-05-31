51d6253f21d77e855c6ef56bfe791e05e31cb4aa
Visha765 <lewis.ap75@gmail.com>
2024-05-31 15:46:44 +0900

    feat: change auto scroll

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_l7/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_l7/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 4);
+    keyball_set_scroll_mode(get_highest_layer(state) == 6);
     return state;
 }
 
