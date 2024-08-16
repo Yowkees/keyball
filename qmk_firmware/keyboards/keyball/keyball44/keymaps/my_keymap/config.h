e893f751e18bd2f2fb2b82afe43cff7b674cab9a
suijinn <h.nakshima1993@gmail.com>
2024-08-17 01:53:32 +0900

    [update] マウスキーを押すまでAutoMouseLayerに留まる ・以下内容をそのまま取り込んだ https://github.com/negokaz/keyball/pull/2

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_keymap/config.h
@@ -37,3 +37,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
+#define AUTO_MOUSE_LAYER_KEEP_TIME 30000
