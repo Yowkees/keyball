95494e79215a46cb152f0c22da887d1004cb77c4
HNH-NASA <selecao.no.13@gmail.com>
2024-08-17 21:42:38 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_keymap/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
+//#    ifdef RGBLIGHT_ENABLE
 //#    define RGBLIGHT_EFFECT_BREATHING
 //#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
 //#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
-#endif
+//#    endif
 
 #define TAP_CODE_DELAY 5
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
