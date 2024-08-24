2bea6d67142684f5b40e1f50a102e123b1a63b0f
suijinn <h.nakshima1993@gmail.com>
2024-08-25 00:35:38 +0900

    [fix] タップ/ホールドの設定変更 ・ホールドキー押上時にタップ判定されたくないのでRETRO_TAPPINGを削除 ・PERMISSIVE_HOLDを設定したのでTAPPING_TERMを長めの300に変更

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_keymap/config.h
@@ -40,4 +40,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000
 
 #define PERMISSIVE_HOLD
-#define RETRO_TAPPING
+#define TAPPING_TERM 300
