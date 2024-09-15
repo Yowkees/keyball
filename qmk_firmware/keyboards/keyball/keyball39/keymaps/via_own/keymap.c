f700f4f2a446bf34f054c41e8aa5337e74d9146f
kentaro <murakami.kentaro.514@gmail.com>
2024-09-15 11:22:12 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via_own/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via_own/keymap.c
@@ -57,7 +57,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
 
-    state = update_tri_layer_state(state, 1, 2, 3);
+    // state = update_tri_layer_state(state, 1, 2, 3);
 
     uint8_t layer = biton32(state);
     switch (layer) {
