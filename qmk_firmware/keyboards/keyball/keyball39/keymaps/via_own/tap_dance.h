b1583add6cd687c62516aef11b5744a2ee2af360
kentaro <murakami.kentaro.514@gmail.com>
2024-09-15 19:16:04 +0900

    tap dance test

---
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via_own/tap_dance.h
@@ -0,0 +1,20 @@
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  DOUBLE_SINGLE_TAP = 5, //send two single taps
+  TRIPLE_TAP = 6,
+  TRIPLE_HOLD = 7
+};
+
+int cur_dance (qk_tap_dance_state_t *state);
+
+//for the x tap dance. Put it here so it can be used in any keymap
+void esc_finished (qk_tap_dance_state_t *state, void *user_data);
+void esc_reset (qk_tap_dance_state_t *state, void *user_data);
