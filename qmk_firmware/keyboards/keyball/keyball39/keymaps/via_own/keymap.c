d3a55e2b54e5de65990f9f7fae613785d0c81aae
kentaro <murakami.kentaro.514@gmail.com>
2024-09-16 22:40:28 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via_own/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via_own/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT_LAYER] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , TD(LCLICK_MLAYER)  , KC_DOT   , KC_SLSH  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , TD(LCLICK_MLAYER)  , TD(RCLICK_NEWTAB)   , KC_SLSH  ,
     TD(ESC_CTL)  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, TD(ESC_CTL)
   ),
 
