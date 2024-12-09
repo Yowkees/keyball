67341b17245a37da263ed489b6f6d1a3a1bd7d9a
shotaCoffee <dripcoffee1018@gmail.com>
2024-12-09 15:44:30 +0900

    fix: bugs

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/viamy/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/viamy/config.h
@@ -20,20 +20,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
+//#ifdef RGBLIGHT_ENABLE
 //#    define RGBLIGHT_EFFECT_BREATHING
 //#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
 //#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
-#endif
+//#endif
 
 #define TAP_CODE_DELAY 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+
+#define KEYBALL_CPI_DEFAULT 700       // 光学センサーPMW3360DM の解像度 (CPI) の規定値
+#define KEYBALL_SCROLL_DIV_DEFAULT 4  // スクロール速度の規定値
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+// ファームウェアのサイズを削減
+#define NO_ACTION_ONESHOT  // 392バイト
+
+#define IGNORE_MOD_TAP_INTERRUPT  // 10バイト
+// #define TAPPING_FORCE_HOLD  // 34バイト
+// 下記三つで444バイト
+#define TAPPING_TERM 175
