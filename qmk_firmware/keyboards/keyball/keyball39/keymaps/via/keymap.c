4577317b6b46d9db561005a4e0327ac0951ecba7
Mckennie <k.koizumi50623@gmail.com>
2024-12-17 22:44:38 +0900

    Auto scroll

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
+    Auto enable scroll mode when the highest layer is 3;
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
     return state;
 }
