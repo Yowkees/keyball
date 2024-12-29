00b5bf6ef9207038145a3f816354e8c7a3403682
Mckennie <k.koizumi50623@gmail.com>
2024-12-29 23:40:18 +0900

    modify_modifier

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_W     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LCTL  , KC_TAB(KC_LGUI)  , KC_LALT  ,LSFT_T(KC_LNG2),LT(3,KC_SPC),LT(2,KC_LNG1),KC_BSPC,LT(1,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT(KC_ESC)
+    KC_LCTL  , LGUI_T(KC_TAB)  , KC_LALT  ,LSFT_T(KC_LNG2),LT(3,KC_SPC),LT(2,KC_LNG1),KC_BSPC,LT(1,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, RSFT_T(KC_ESC)
   ),
 
   [1] = LAYOUT_universal(
     KC_AT    , KC_7    , KC_8    , KC_9    , KC_QUOT  ,                            KC_LCBR    ,   KC_EQL  , KC_TILD    , KC_PIPE    ,  KC_RCBR  ,
     KC_EXLM    , KC_4    , KC_5     , KC_6    , KC_DQUO  ,                           KC_LPRN, KC_PLUS  , KC_UNDS  , KC_SCLN     , KC_RPRN  ,
     S(KC_SLSH) , KC_1    , KC_2     , KC_3    , KC_EQL,                            KC_LBRC  , KC_ASTR   , KC_PERC  , KC_COLN  , KC_RBRC   ,
-    KC_DLR  , KC_0   , S(KC_MIN)  , _______  , _______  , _______  ,      TO(2)    , TO(0)   , _______  , KC_RALT  , KC_RGUI  , KC_HASH
+    KC_DLR  , KC_0   , S(KC_MINS)  , _______  , _______  , _______  ,      TO(2)    , TO(0)   , _______  , KC_RALT  , KC_RGUI  , KC_HASH
   ),
 
   [2] = LAYOUT_universal(
