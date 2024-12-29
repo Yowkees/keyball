c50a8709ecc22e698a9c207d25d618b98c02f8d0
Mckennie <k.koizumi50623@gmail.com>
2024-12-29 23:28:29 +0900

    first version

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -27,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_W     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LCTL  , KC_TAB  , KC_LALT  ,LSFT_T(KC_LNG2),LT(3,KC_SPC),LT(2,KC_LNG1),KC_BSPC,LT(1,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
+    KC_LCTL  , KC_TAB(KC_LGUI)  , KC_LALT  ,LSFT_T(KC_LNG2),LT(3,KC_SPC),LT(2,KC_LNG1),KC_BSPC,LT(1,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT(KC_ESC)
   ),
 
   [1] = LAYOUT_universal(
-    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
-    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
-    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
-    KC_INT1  , KC_EQL   , S(KC_3)  , _______  , _______  , _______  ,      TO(2)    , TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
+    KC_AT    , KC_7    , KC_8    , KC_9    , KC_QUOT  ,                            KC_LCBR    ,   KC_EQL  , KC_TILD    , KC_PIPE    ,  KC_CBR  ,
+    KC_EXLM    , KC_4    , KC_5     , KC_6    , KC_DQUO  ,                           KC_LPRN, KC_PLUS  , KC_UNDS  , KC_SCLN     , KC_RPRN  ,
+    S(KC_SLSH) , KC_1    , KC_2     , KC_3    , KC_EQL,                            KC_LBRC  , KC_ASTR   , KC_PERC  , KC_COLN  , KC_RBRC   ,
+    KC_DLR  , KC_0   , S(KC_MIN)  , _______  , _______  , _______  ,      TO(2)    , TO(0)   , _______  , KC_RALT  , KC_RGUI  , KC_HASH
   ),
 
   [2] = LAYOUT_universal(
-    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
-   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
-    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
-    KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
+    KC_PSCR   , KC_F7     , KC_F8     , KC_F9     , KC_LSFT,                     KC_ESC  , SSNP_VRT  , SSNP_HOR  , SSNP_FRE  , KC_BSPC  ,
+   KC_TAB , KC_F4     , KC_F5     , KC_F6     , KC_LCTL,                           SCRL_TO  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
+    KC_LSFT  , KC_F1     , KC_F2     , KC_F3     , KC_LGUI ,                           KC_HOME, KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_END ,
+    KC_LCTL   , KC_0     , KC_LALT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , KC_ESC
   ),
 
   [3] = LAYOUT_universal(
@@ -78,6 +78,8 @@ void oledkit_render_info_user(void) {
 #endif
 
 const uint16_t PROGMEM combo1[] = {LT(1,KC_ENT), LT(2,KC_LNG1), COMBO_END};
+const uint16_t PROGMEM combo2[] = {KC_PIPE, KC_CBR, COMBO_END};
 combo_t key_combos[] = {
     COMBO(combo1, TT(4)),
+    COMBO(combo2, KC_BSLS),
 };
