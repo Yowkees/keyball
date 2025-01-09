c371456efeabe9418af3e9531d0c1ddfef4e4037
Mckennie <k.koizumi50623@gmail.com>
2025-01-09 21:36:35 +0900

    change number

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -27,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    LCTL_T(KC_TAB)  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_SPC),LT(3,KC_ENT),LT(2,KC_BSPC),LT(1,KC_LNG1),KC_LNG2,LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, RSFT_T(KC_ESC)
+    LCTL_T(KC_TAB)  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_BSPC),LT(3,KC_ENT),LT(2,KC_SPC),LT(1,KC_LNG2),KC_LNG2,LSFT_T(KC_LNG1),KC_RALT,KC_RGUI, RSFT_T(KC_ESC)
   ),
 
   [1] = LAYOUT_universal(
-    KC_LBRC    , KC_7    , KC_8    , KC_9    , S(KC_2)  ,                            S(KC_RBRC)    ,   KC_INT1  , S(KC_6)    , S(KC_INT3)    ,  S(KC_BSLS)  ,
-    KC_EXLM    , KC_4    , KC_5     , KC_6    , S(KC_7)  ,                           S(KC_8), S(KC_SCLN)  , S(KC_INT1)  , KC_SCLN     , S(KC_9)  ,
-    KC_DLR , KC_1    , KC_2     , KC_3    , KC_EQL,                            KC_RBRC  , KC_DQUO   , KC_PERC  , KC_QUOT  , KC_BSLS   ,
-    KC_HASH  , KC_0   , S(KC_EQL)  , _______  , _______  , TO(2)  ,       _______   , TO(0)   , _______  , KC_RALT  , KC_RGUI  , S(KC_LBRC)
+    KC_1    , KC_2     , KC_3    , KC_LBRC   , S(KC_2)  ,                            S(KC_RBRC)    ,   KC_INT1  , S(KC_6)    , S(KC_INT3)    ,  S(KC_BSLS)  ,
+    KC_4    , KC_5     , KC_6    , KC_EXLM   ,  S(KC_7)  ,                           S(KC_8), S(KC_SCLN)  , S(KC_INT1)  , KC_SCLN     , S(KC_9)  ,
+    KC_7    , KC_8     , KC_9    , KC_DLR    ,  KC_EQL,                            KC_RBRC  , KC_DQUO   , KC_PERC  , KC_QUOT  , KC_BSLS   ,
+    KC_0    , KC_HASH  , S(KC_EQL)  , _______  , _______  , TO(2)  ,       _______   , TO(0)   , _______  , KC_RALT  , KC_RGUI  , S(KC_LBRC)
   ),
 
   [2] = LAYOUT_universal(
-    KC_PSCR   , KC_F7     , KC_F8     , KC_F9     , KC_LSFT,                     KC_ESC  , SSNP_VRT  , SSNP_HOR  , SSNP_FRE  , KC_BSPC  ,
-   KC_TAB , KC_F4     , KC_F5     , KC_F6     , KC_LCTL,                           SCRL_TO  , KC_BTN1  , KC_UP    , KC_BTN2  , TO(2) ,
-    KC_LSFT  , KC_F1     , KC_F2     , KC_F3     , KC_LGUI ,                           KC_HOME, KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_END ,
-    KC_LCTL   , KC_LGUI     , KC_LALT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      TO(1)  , TO(0)  , _______  , _______  , _______  , RSFT_T(KC_ESC)
+    KC_PSCR   , KC_F2     , KC_F3     , KC_C     , KC_V,                     KC_X  , SSNP_VRT  , SSNP_HOR  , SSNP_FRE  , KC_BSPC  ,
+    KC_F4     , KC_F5     , KC_F6     , KC_TAB  , KC_LATL,                           SCRL_TO  , KC_BTN1  , KC_UP    , KC_BTN2  , TO(2) ,
+    KC_F7     , KC_F8     , KC_F9     , KC_LSFT  , KC_LGUI ,                           KC_HOME, KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_END ,
+    KC_F10   , KC_F11     , KC_F12   , KC_BSPC   , KC_ENT   , KC_LCTL  ,      TO(1)  , TO(0)  , _______  , _______  , _______  , RSFT_T(KC_ESC)
   ),
 
   [3] = LAYOUT_universal(
