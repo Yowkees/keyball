1b28b010d393e8a9082e7dbdc4c9f94ca9ae7258
rm86choppat <166978804+rm86choppat@users.noreply.github.com>
2025-01-26 20:39:25 +0900

    add_layer2 caps_lock押しながら矢印キー操作をするため、レイヤー追加

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/miura/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/miura/config.h
@@ -38,4 +38,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
