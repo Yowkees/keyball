27b74eb10fd86453b32124a297960a2234c04be5
Kenji Ito <lzenzy@gmail.com>
2025-03-10 23:51:49 +0900

    Combo esc, tab, bs and mouse keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/lzenzy/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/lzenzy/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_INT3  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
     MO(1)    , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-    _______  , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),    KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_RALT  , KC_PSCR
+    _______  , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),LT(2,KC_SPC),LT(3,KC_LNG1),    KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_RALT  , LT(2,KC_ENT)
   ),
 
   [1] = LAYOUT_universal(
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_universal(
     SSNP_FRE , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     SSNP_VRT , _______  , KC_7     , KC_8     , KC_9     , _______  ,                                  _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
-    SSNP_HOR , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  , _______  ,
+    SSNP_HOR , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , LT(3,KC_BTN2) , KC_DOWN  , KC_BTN3  , _______  ,
     _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             KC_DEL  , _______  , _______  , _______  , _______  , _______  , _______
   ),
@@ -76,17 +76,15 @@ void oledkit_render_info_user(void) {
 #ifdef COMBO_ENABLE
 #include "combo.h"
 
-// コンボの識別子
-enum combos {
-  COMBO_ESC,  // Q + W を押すとESC
-  COMBO_COUNT
-};
-
-// コンボとなるキーの配列
-const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
+// コンボとなるキーの配列[
+const uint16_t PROGMEM combo_esc[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_bs[] = {KC_O, KC_P, COMBO_END};
 
 // コンボキーの配列
-combo_t key_combos[COMBO_COUNT] = {
-  COMBO(combo_esc, KC_ESC)
+combo_t key_combos[] = {
+  COMBO(combo_esc, KC_ESC),
+  COMBO(combo_tab, KC_TAB),
+  COMBO(combo_tab, KC_BSPC)
 };
 #endif
