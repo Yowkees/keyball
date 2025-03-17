6768f8f70331d401fa7eb80e3f14adc7ef966d4f
Kenji Ito <lzenzy@gmail.com>
2025-03-17 12:23:05 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/lzenzy/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/lzenzy/keymap.c
@@ -62,17 +62,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-#ifdef OLED_ENABLE
-
-#    include "lib/oledkit/oledkit.h"
-
-void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
-}
-#endif
-
 #ifdef COMBO_ENABLE
 
 
