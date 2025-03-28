98da592fc1f92e23ae981dc76459033d7fd8fb2e
KenjiIto <lzenzy@gmail.com>
2025-03-28 18:58:24 +0900

    adjust Tapping Term

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/lzenzy/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/lzenzy/config.h
@@ -40,3 +40,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define COMBO_ONLY_FROM_LAYER 0
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
