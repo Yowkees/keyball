55600bf49870f34a96f3989c04d67ccb50ea3e9e
we1per <na0k1kuwa71@gmail.com>
2025-05-11 01:49:41 +0900

    レイヤー設定変更
    ・レイヤー数Up
    ・AMLをレイヤー6へ変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -34,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAP_CODE_DELAY 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7  // レイヤー数を7へ増加
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_DEFAULT_LAYER 6  // AMLはレイヤー6にする
