7d506c54f0b4dd8681ef5fd2ab965904e2054313
we1per <na0k1kuwa71@gmail.com>
2025-05-11 01:48:29 +0900

    各種設定有効化
    以下3つ
    ・EXTRAKEY
    ・CAPS_WORD
    ・DYNAMIC_TAPPING_TERM

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/rules.mk
@@ -3,3 +3,9 @@ RGBLIGHT_ENABLE = yes
 OLED_ENABLE = yes
 
 VIA_ENABLE = yes
+
+EXTRAKEY_ENABLE = yes  # 約490bytes
+
+CAPS_WORD_ENABLE = yes  # 約470bytes
+
+DYNAMIC_TAPPING_TERM_ENABLE = yes  # 約270bytes
