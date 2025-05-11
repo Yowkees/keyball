58b2647acad7ee2fa821a0b879f9e8db1ccf687e
keiko-yellow <freetoyoyu@gmail.com>
2025-05-11 15:20:14 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -37,3 +37,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
