6ce6c57415be9ba3a5172b83c6faa0a880834cce
takashi matsunaga <101456528+shun0823dane@users.noreply.github.com>
2025-07-24 19:48:20 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/develop/config.h
@@ -31,3 +31,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_EFFECT_ALTERNATING
 #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
