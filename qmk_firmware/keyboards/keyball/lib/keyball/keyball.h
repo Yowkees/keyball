bcfe25225b107d7220eea7c7280911c4f2940c92
harappa <navisto.equation@gmail.com>
2025-10-09 00:10:59 +0900

    速度調整

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.h
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.h
@@ -21,11 +21,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Configurations
 
 #ifndef KEYBALL_CPI_DEFAULT
-#    define KEYBALL_CPI_DEFAULT 800    /* 2025-10-06_500→400 */
+#    define KEYBALL_CPI_DEFAULT 600    /* 2025-10-06_500→400 */
 #endif
 
 #ifndef KEYBALL_SCROLL_DIV_DEFAULT
-#    define KEYBALL_SCROLL_DIV_DEFAULT 6 // 4: 1/8 (1/2^(n-1))
+#    define KEYBALL_SCROLL_DIV_DEFAULT 7 // 4: 1/8 (1/2^(n-1))
 #endif
 
 #ifndef KEYBALL_REPORTMOUSE_INTERVAL
