0d0180e045d31776ad65d0fb80f3a59b88325481
kzkzk96 <zodia9ue@gmail.com>
2025-10-10 16:41:05 +0900

    Create config.h

---
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/kzkzk/config.h
@@ -0,0 +1,60 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#ifdef RGBLIGHT_ENABLE
+//#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_SNAKE
+//#    define RGBLIGHT_EFFECT_KNIGHT
+//#    define RGBLIGHT_EFFECT_CHRISTMAS
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_RGB_TEST
+//#    define RGBLIGHT_EFFECT_ALTERNATING
+//#    define RGBLIGHT_EFFECT_TWINKLE
+#endif
+
+// --- タップ系設定 ---
+#define TAPPING_TERM 180     // 長押しとタップの判定時間 (ms)
+// #define IGNORE_MOD_TAP_INTERRUPT //長押し判定前に次の入力がきたら、タップ判定
+#define TAP_CODE_DELAY 5 //キー押下と離すの間のWait時間（デフォルト:０）※tap_code() / SEND_STRING() / マクロ内の疑似キー入力にだけ影響
+
+// --- チャタリング対策 ---
+#undef DEBOUNCE
+#define DEBOUNCE 10          // デフォルト5ms → 10msに延長
+
+// --- 自動マウス機能 ---不要なのでコメントアウト
+// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
+// #define AUTO_MOUSE_DEFAULT_LAYER 1 // 切り替えるマウスレイヤー番号を指定
+// #define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
+
+// --- マウス設定 ---
+#define KEYBALL_CPI_DEFAULT 500         // マウス速度 (default: 500)
+#define KEYBALL_SCROLL_DIV_DEFAULT 5    // スクロール速度 (default: 4)
+
+// --- レイヤ追加 ---
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6    // デフォルト4 → 6に拡張
+
+
+// --- コンボ設定 ---
+#define COMBO_TERM 30   //同時押し判定用（30〜40ms程度がおすすめ）
+#define COMBO_ALLOW_ACTION_KEYS //「QMKが安全のためブロックする」動作の防止を明示　※なくても問題はなし
