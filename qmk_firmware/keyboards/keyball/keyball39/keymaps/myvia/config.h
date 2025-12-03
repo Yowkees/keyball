61d7d27903fa96ddce4075d3c6d43cc036f0a666
Hiroki Itoh <ihiroky@gmail.com>
2025-12-03 23:49:42 +0900

    chore: Remove unused defines

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/myvia/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/myvia/config.h
@@ -25,19 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_AUTO_SHIFT_SPECIAL
 #define NO_ACTION_ONESHOT
 
-#ifdef RGBLIGHT_ENABLE
-//#    define RGBLIGHT_EFFECT_BREATHING
-//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-//#    define RGBLIGHT_EFFECT_KNIGHT
-//#    define RGBLIGHT_EFFECT_CHRISTMAS
-//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//#    define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_EFFECT_ALTERNATING
-//#    define RGBLIGHT_EFFECT_TWINKLE
-#endif
-
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
