034f4cb1a466d28b1eeb40665b3e4fbf73fe2662
ikeiked <ysk.ikeda@gmail.com>
2026-02-14 23:19:08 +0900

    レイヤーを追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -37,3 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+// VIAで使用できるレイヤー数を6に設定
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
