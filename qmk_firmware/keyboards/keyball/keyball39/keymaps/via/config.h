bb0eb0fc309de04c1e67d8bc3ad38629ffbef14d
yusuke1225math2 <63696752+yusuke1225math2@users.noreply.github.com>
2026-03-20 21:06:48 +0900

    typo

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -39,4 +39,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 
 // レイヤー追加
-#define DYNAMIC_KEYMAP_LAYER_COUNT = 8
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
